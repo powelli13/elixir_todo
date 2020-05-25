630017b9354025fdec141a36fedc7eaa9cf82a85
Isaac Powell <powelli91@outlook.com>
2020-05-25 14:04:44 -0700

    implemented stateful concurrent todo server

--- todo_server.ex
+++ todo_server.ex
@@ -82,3 +82,58 @@ defmodule TodoList.CsvImporter do
         |> TodoList.new()
     end
 end
+
+defmodule TodoServer do
+    def start() do
+        spawn(fn -> loop(TodoList.new()) end)
+    end
+
+    def add_entry(todo_server, new_entry) do
+        send(todo_server, {:add_entry, new_entry})
+    end
+
+    def update_entry(todo_server, id, updater) do
+        send(todo_server, {:update_entry, id, updater})
+    end
+
+    def delete_entry(todo_server, id) do
+        send(todo_server, {:delete_entry, id})
+    end
+
+    def entries(todo_server, date) do
+        send(todo_server, {:entries, self(), date})
+
+        receive do
+            {:todo_entries, entries} -> entries
+        after
+            5000 -> {:error, :timeout}
+        end
+    end
+
+    defp process_message(todo_list, {:add_entry, new_entry}) do
+        TodoList.add_entry(todo_list, new_entry)
+    end
+
+    defp process_message(todo_list, {:update_entry, id, updater}) do
+        TodoList.update_entry(todo_list, id, updater)
+    end
+
+    defp process_message(todo_list, {:delete_entry, id}) do
+        TodoList.delete_entry(todo_list, id)
+    end
+
+    defp process_message(todo_list, {:entries, caller, date}) do
+        send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+        todo_list
+    end
+
+    defp loop(todo_list) do
+        new_todo_list = 
+            receive do
+                message ->
+                    process_message(todo_list, message)
+            end
+
+        loop(new_todo_list)
+    end
+end
