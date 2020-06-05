735881fb721f03d7c4c0a98c9fe32c8480222965
Isaac Powell <isaac.powell@sprbrk.com>
2020-06-05 14:53:40 -0700

    GenServer learning in chpt 6. still need to do end of chapter exercise.

---
+++ key_value_gen_server.ex
@@ -0,0 +1,32 @@
+defmodule KeyValueStore do
+    use GenServer
+
+    # interface functions
+    def start do
+        GenServer.start(KeyValueStore, nil, name: __MODULE__)
+    end
+
+    def put(key, value) do
+        GenServer.cast(__MODULE__, {:put, key, value})
+    end
+
+    def get(key) do
+        GenServer.call(__MODULE__, {:get, key})
+    end
+
+    # callback functions used by GenServer behaviour
+    @impl GenServer
+    def init(_) do
+        {:ok, %{}}
+    end
+
+    @impl GenServer
+    def handle_cast({:put, key, value}, state) do
+        {:noreply, Map.put(state, key, value)}
+    end
+
+    @impl GenServer
+    def handle_call({:get, key}, _, state) do
+        {:reply, Map.get(state, key), state}
+    end
+end
